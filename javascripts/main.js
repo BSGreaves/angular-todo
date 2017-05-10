cc967b3d68cdb12a30a38cc09a9dd89e4a847019
Ben Greaves <bsgreaves@gmail.com>
2017-05-09 19:48:19 -0500

    Got a dynamic navbar, that's pretty cool

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
-$(function() {
-    console.log( "ready!" );
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", ($scope) => {
+	$scope.cat = "Meow";
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof!";
 });
