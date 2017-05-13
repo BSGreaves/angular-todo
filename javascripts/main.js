d36062dd1ec90d18400a33017d6536d73ff0d832
Ben Greaves <bsgreaves@gmail.com>
2017-05-13 11:43:03 -0500

    Reading from fb

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,25 +1,52 @@
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(FIREBASE_CONFIG);
+});
+
 app.controller("NavCtrl", ($scope) => {
-    $scope.cat = "Meow";
-    $scope.navItems = [{
-        name: "Logout"
-    }, {
-        name: "All Items"
-    }, {
-        name: "New Item"
-    }];
+	$scope.cat = "Meow";
+		$scope.navItems = [{
+				name: "Logout"
+		}, {
+				name: "All Items"
+		}, {
+				name: "New Item"
+		}];
 });
 
-app.controller("ItemCtrl", ($scope) => {
-    $scope.dog = "Woof!";
-    $scope.showListView = true;
-    $scope.items = [];
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+		$scope.dog = "Woof!";
+		$scope.showListView = true;
+		$scope.items = [];
 
-    $scope.newItem = () => {
-        $scope.showListView = false;
-    };
+		$scope.newItem = () => {
+				$scope.showListView = false;
+		};
 
-    $scope.allItems = () => {
-        $scope.showListView = true;
-    };
+		$scope.allItems = () => {
+				$scope.showListView = true;
+		};
+
+		let getItemList = () => {
+			let items = [];
+			return $q((resolve, reject) => {
+				$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+				.then(fbItems => {
+					fbItems = fbItems.data;
+          Object.keys(fbItems).forEach((key) => {
+            fbItems[key].id=key;
+            items.push(fbItems[key]);
+          	resolve(items);
+          });
+				})
+				.catch(error => {reject(error);});
+			});
+		};
+
+		let getItems = () => {
+			getItemList()
+			.then(items => {$scope.items = items})
+			.catch(error => console.log("Error - getItems", error));
+		};
+
+		getItems();
 });
-
