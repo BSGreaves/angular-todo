5586fa21be46c3d3d0c1c2cfff4a9cfb4bf3ea37
Ben Greaves <bsgreaves@gmail.com>
2017-05-13 12:26:45 -0500

    Add to-do working

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,39 +14,57 @@ app.controller("NavCtrl", ($scope) => {
 });
 
 app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
-		$scope.dog = "Woof!";
-		$scope.showListView = true;
-		$scope.items = [];
+	$scope.dog = "Woof!";
+	$scope.showListView = true;
+	$scope.items = [];
 
-		$scope.newItem = () => {
-				$scope.showListView = false;
-		};
+	$scope.newItem = () => {
+			$scope.showListView = false;
+	};
 
-		$scope.allItems = () => {
-				$scope.showListView = true;
-		};
+	$scope.allItems = () => {
+			$scope.showListView = true;
+	};
 
-		let getItemList = () => {
-			let items = [];
-			return $q((resolve, reject) => {
-				$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
-				.then(fbItems => {
-					fbItems = fbItems.data;
-          Object.keys(fbItems).forEach((key) => {
-            fbItems[key].id=key;
-            items.push(fbItems[key]);
-          	resolve(items);
-          });
-				})
-				.catch(error => {reject(error);});
-			});
-		};
+	let getItemList = () => {
+		let items = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+			.then(fbItems => {
+				fbItems = fbItems.data;
+        Object.keys(fbItems).forEach((key) => {
+          fbItems[key].id=key;
+          items.push(fbItems[key]);
+        	resolve(items);
+        });
+			})
+			.catch(error => {reject(error);});
+		});
+	};
 
-		let getItems = () => {
-			getItemList()
-			.then(items => {$scope.items = items})
-			.catch(error => console.log("Error - getItems", error));
-		};
+	let getItems = () => {
+		getItemList()
+		.then(items => {$scope.items = items;})
+		.catch(error => console.log("Error - getItems", error));
+	};
 
-		getItems();
+	getItems();
+
+	let postNewItem = (newItem) => {
+		return $q((resolve, reject) => {
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
+			.then(result => resolve(result))
+			.catch(error => reject(error));
+		});
+	};
+
+	$scope.addNewItem = () => {
+		$scope.newTask.isCompleted = false;
+		postNewItem($scope.newTask).then((response) => {
+			$scope.showListView = true;
+			$scope.newTask = {};
+			getItems();
+		}).catch(error => console.log("error in addNewItem", error));
+	};
+		
 });
