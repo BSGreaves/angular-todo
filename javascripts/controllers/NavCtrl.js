b1ff9ca7cb0b7e56487a09d9cdd3e1b1b69f8517
Ben Greaves <bsgreaves@gmail.com>
2017-05-16 18:39:42 -0500

    Nav bar partitioned out

--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -1,10 +1,3 @@
 app.controller("NavCtrl", ($scope) => {
-	$scope.cat = "Meow";
-		$scope.navItems = [{
-				name: "Logout"
-		}, {
-				name: "All Items"
-		}, {
-				name: "New Item"
-		}];
+
 });
